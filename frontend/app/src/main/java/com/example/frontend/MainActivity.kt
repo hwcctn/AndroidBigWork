b4105565ceb581e21aa9457c6fb819d6d00df6be
hwcctn <2738268497@qq.com>
2024-09-06 01:40:55 +0800

    登录注册底部导航栏

--- frontend/app/src/main/java/com/example/frontend/MainActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -1,20 +1,90 @@
 package com.example.frontend
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+    private var selectedTab: Int = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //绑定控件和布局
+        val hotLayout: LinearLayout = findViewById(R.id.hotLayout)
+        val dynamicLayout: LinearLayout = findViewById(R.id.dynamicLayout)
+        val personLayout: LinearLayout = findViewById(R.id.personLayout)
+
+        var hotText: TextView = findViewById(R.id.hot_tx);
+        var dynamicText: TextView = findViewById(R.id.dynamic_tx);
+        var personText: TextView = findViewById(R.id.person_tx);
+        //设置默认界面为HotFragment
+        supportFragmentManager.beginTransaction()
+            .setReorderingAllowed(true)
+            .replace(R.id.fragment_container,HotFragment())
+            .commit()
+
+        hotLayout.setOnClickListener {
+            // 检查是否被选中
+            if (selectedTab != 1) {
+                //设置界面
+                supportFragmentManager.beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment_container,HotFragment())
+                    .commit()
+
+                // 设置其他按钮为未选中状态
+                dynamicLayout.setBackgroundResource(R.drawable.front_border);
+                personLayout.setBackgroundResource(R.drawable.front_border);
+                // 设置 home 按钮的选中状态
+                hotLayout.setBackgroundResource(R.drawable.front_border_select);
+                selectedTab = 1
+            }
         }
+        dynamicLayout.setOnClickListener {
+            // 检查是否被选中
+            if (selectedTab != 2) {
+                //设置界面
+                supportFragmentManager.beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment_container, DynamicFragment())
+                    .commit()
+
+                // 设置其他按钮为未选中状态
+                hotLayout.setBackgroundResource(R.drawable.front_border);
+                personLayout.setBackgroundResource(R.drawable.front_border);
+                // 设置 home 按钮的选中状态
+                dynamicLayout.setBackgroundResource(R.drawable.front_border_select);
+                selectedTab = 2
+            }
+        }
+        personLayout.setOnClickListener {
+            // 检查是否被选中
+            if (selectedTab != 3) {
+                //设置界面
+                supportFragmentManager.beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment_container, PersonFragment())
+                    .commit()
+
+                // 设置其他按钮为未选中状态
+                hotLayout.setBackgroundResource(R.drawable.front_border);
+                dynamicLayout.setBackgroundResource(R.drawable.front_border);
+                // 设置 home 按钮的选中状态
+                personLayout.setBackgroundResource(R.drawable.front_border_select);
+                selectedTab = 3
+            }
+        }
+
+
     }
+
+
 }
---
+++ frontend/app/src/main/java/com/example/frontend/PersonFragment.kt
@@ -0,0 +1,16 @@
+package com.example.frontend;
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+public class PersonFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_person, container, false)
+    }
+}
